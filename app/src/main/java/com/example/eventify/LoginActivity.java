aa54d4ffdacd56b381b416bd11dbec18dddd291d
dejan <31327026+Lija321@user.noreply.github.com>
2024-11-11 18:40:41 +0100

    changed the name and the icons of the app

---
+++ app/src/main/java/com/example/eventify/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.eventify;
+
+import android.annotation.SuppressLint;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+import com.example.eventify.databinding.ActivityLoginBinding;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
